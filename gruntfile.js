b63bb223a210409d8a2c961ce7c1a124cf1858ee
Subhas Dandapani <rdsubhas@gmail.com>
2015-10-20 18:34:41 +0530

    Add OSX builder

---
+++ gruntfile.js
@@ -0,0 +1,24 @@
+module.exports = function (grunt) {
+  require('load-grunt-tasks')(grunt)
+
+  grunt.initConfig({
+
+    electron: {
+      osx: {
+        options: {
+          name: 'Docker Menu',
+          dir: '.',
+          asar: true,
+          overwrite: true,
+          out: 'build',
+          platform: 'darwin',
+          version: '0.34.0',
+          arch: 'x64'
+        }
+      }
+    }
+
+  })
+
+  grunt.registerTask('default', ['electron'])
+}
